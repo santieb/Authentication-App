fb1a7c3827532a398fe3488fc3ddf6a7fe6d8b44
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-12-29 18:55:30 -0300

    [add] user.model

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please, enter your name!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please, enter your email!"],
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, "Please, enter your password!"],
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  avatar: {
+    type: String,
+    default: ""
+  }
+}, {
+  timestamps: true
+})
+
+export default mongoose.model("Users", userSchema)
