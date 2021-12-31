56c31cac292a433db129db90525e72cb972eb642
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-12-31 20:26:07 -0300

    [add] helmet and morgan

---
+++ index.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('tiny'))
+
+const URI = process.env.MONGODB_URL
+
+mongoose.connect(
+  URI,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  (err) => {
+    if (err) throw err
+    console.log('Mongodb connection')
+  }
+)
+
+import userRoutes from './users/routes/user.routes'
+app.use('/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
